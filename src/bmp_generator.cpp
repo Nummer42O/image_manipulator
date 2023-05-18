604f6cd9f2e080b9e2d9bb07e622380f08342d52
Nummer42O <johannes.handy007@gmail.com>
2023-05-18 21:33:22 +0200

    adopted generator for removed color spaces

--- src/bmp_generator.cpp
+++ src/bmp_generator.cpp
@@ -5,10 +5,9 @@
 
 const std::array<const std::array<const double, NR_CHANNELS>, image_proc::ColorSpace::LAST> color_space_base_layouts {{
     {{0.0, 0.0, 0.0}},
-    {{0.0, 0.0, 0.0}},          {{0.0, 0.0, 0.0}},          {{0.0, 0.0, 0.0}},          {{0.5, 0.5, 0.5}},          {{0.5, 0.5, 0.5}},
-    /*                          Not the best solution for HSV/HLS but better not to complex either                                  */
-    {{0.5, 0.5, 0.5}},          {{0.5, 0.5, 0.5}},          {{0.5, 0.5, 0.5}},          {{0.5, 0.5, 0.5}},          {{0.0, 0.0, 0.0}},
-    {{0.0, 0.0, 0.0}}
+    {{0.0, 0.0, 0.0}},          {{0.0, 0.0, 0.0}},          {{0.5, 0.5, 0.5}},          {{0.5, 0.5, 0.5}},
+    /*                          Not the best solution for HSV/HLS but better not to complex either      */
+    {{0.5, 0.5, 0.5}},          {{0.5, 0.5, 0.5}},          {{0.5, 0.5, 0.5}},          {{0.5, 0.5, 0.5}},
 }};
 const std::string default_save_location = "../resources/";
 
@@ -42,44 +41,28 @@ int main() {
     for (size_t i = 0ul; i < image_proc::ColorSpace::LAST; i++) {
         conversion_code = image_proc::convert_to_rgb[i];
         save_location_base_name = default_save_location + image_proc::color_space_names[i] + '_';
+        
+        channels[0].setTo(cv::Scalar(255.0 * color_space_base_layouts[i][0]));
+        channels[1].setTo(cv::Scalar(255.0 * color_space_base_layouts[i][1]));
+        channels[2].setTo(cv::Scalar(255.0 * color_space_base_layouts[i][2]));
 
-        if (image_proc::color_space_nr_channels[i] == 1ul) {
+        for (size_t channel_idx = 0ul; channel_idx < NR_CHANNELS; channel_idx++) {
+            channel_backup = channels[channel_idx];
+            channels[channel_idx] = ranged_channel;
+
+            cv::merge(channels, output);
             if (conversion_code != cv::COLOR_COLORCVT_MAX) {
-                cv::cvtColor(ranged_channel, output, conversion_code);
-            } else {
-                ranged_channel.copyTo(output);
+                cv::cvtColor(output, output, conversion_code);
             }
 
-            //appending the channel is unnecessary but makes it easier to streamline loading them further down the line
-            save_location = save_location_base_name + image_proc::color_space_channels[i][0] + ".bmp";
+            save_location = save_location_base_name + image_proc::color_space_channels[i][channel_idx] + ".bmp";
             if (!cv::imwrite(save_location, output)) {
                 std::cerr << "Unable to save file " << save_location << ". Skipping." << std::endl;
 
                 continue;
             }
-        } else {
-            channels[0].setTo(cv::Scalar(255.0 * color_space_base_layouts[i][0]));
-            channels[1].setTo(cv::Scalar(255.0 * color_space_base_layouts[i][1]));
-            channels[2].setTo(cv::Scalar(255.0 * color_space_base_layouts[i][2]));
 
-            for (size_t channel_idx = 0ul; channel_idx < NR_CHANNELS; channel_idx++) {
-                channel_backup = channels[channel_idx];
-                channels[channel_idx] = ranged_channel;
-
-                cv::merge(channels, output);
-                if (conversion_code != cv::COLOR_COLORCVT_MAX) {
-                    cv::cvtColor(output, output, conversion_code);
-                }
-
-                save_location = save_location_base_name + image_proc::color_space_channels[i][channel_idx] + ".bmp";
-                if (!cv::imwrite(save_location, output)) {
-                    std::cerr << "Unable to save file " << save_location << ". Skipping." << std::endl;
-
-                    continue;
-                }
-
-                channels[channel_idx] = channel_backup;
-            }
+            channels[channel_idx] = channel_backup;
         }
     }
 
